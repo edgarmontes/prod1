29590d192fac771a2147bd26744017254abc1873
edgarmontes <edgarmontes@users.noreply.github.com>
2025-12-15 10:26:55 -0800

    Added function to get all Ids from a chanel

--- video_refactored.py
+++ video_refactored.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 load_dotenv(dotenv_path="./.env")
 api_key = os.getenv("api_key")
 channel_name = os.getenv("channel_name")
+max_results = 50
 
 
 def get_ID():
@@ -22,5 +23,36 @@ def get_ID():
     except requests.exceptions.RequestException as e:
         raise e
     
+
+def get_video_ids(playlistID):
+    video_ids = []
+    pageToken = None
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={max_results}&playlistId={playlistID}&key={api_key}"
+    try:
+        while True:
+            url = base_url
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+
+            for item in data.get("items",[]):
+                video_id = item["contentDetails"]["videoId"]
+                video_ids.append(video_id)
+
+            pageToken = data.get("nextPageToken")
+
+            if not pageToken:
+                break
+
+        return video_ids
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
+
 if __name__ == "__main__":
-    print(get_ID())
+    playlistID = get_ID()
+    print(get_video_ids(playlistID))
